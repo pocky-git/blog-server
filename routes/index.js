e1624eae9c9ffd97becca92814ddb31494db0151
pocky-git <997859540@qq.com>
2020-10-29 22:31:25 +0800

    自动登录接口

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,27 @@ router.post('/login', function(req, res) {
   })
 })
 
+//自动登录接口
+router.get('/user',function(req,res){
+  const _id = req.cookies._id
+  if(!_id){
+    return res.send({code: 1,msg: '请先登录'})
+  }
+  User.findOne(
+    {
+      _id
+    },
+    {
+      password: 0,
+      __v: 0
+    },
+    (err,user) => {
+    if(err){
+      return res.send({code: 500,msg: '服务器错误'})
+    }
+    res.cookie('_id',user._id,{maxAge: 1000*60*60*24})
+    res.send({code: 0,data: user})
+  })
+})
+
 module.exports = router
